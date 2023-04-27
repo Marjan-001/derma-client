0381a9ddeef5dc9c72223f975f5e577610def2bc
Marjan-001 <marjan199721@gmail.com>
2023-04-27 16:01:53 +0600

    navbar

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import logo from '../assests/images/DerMa-removebg-preview.png'
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>this is navbar</h2>
-        </div>
+        <header className="p-1 bg-amber-50 dark:text-gray-100">
+            <div className="container flex justify-between h-16 mx-auto">
+                <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
+                    <img src={logo} className='w-28 h-28 object-cover' alt="" />
+                </a>
+                <ul className="items-stretch hidden space-x-3 lg:flex">
+                    <li className="flex">
+                        <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 hover:border-amber-950 text-amber-950 ">About</a>
+                    </li>
+                    <li className="flex">
+                        <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2  hover:border-amber-950  text-amber-950">Contact</a>
+                    </li>
+                    <li className="flex">
+                        <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2  hover:border-amber-950  text-amber-950">Services</a>
+                    </li>
+                    <li className="flex">
+                        <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2  hover:border-amber-950  text-amber-950">Appointment</a>
+                    </li>
+                </ul>
+                <div className="items-center flex-shrink-0 hidden lg:flex">
+                    <button className="self-center px-8 py-3 rounded">Sign in</button>
+                    <button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Sign up</button>
+                </div>
+                <button className="p-4 lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
+        </header>
     );
 };
 
