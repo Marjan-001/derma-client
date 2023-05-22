996a9ae07f4c3a8a313594a95eaf84916ccfd586
Marjan-001 <marjan199721@gmail.com>
2023-05-22 22:39:33 +0600

    appointmentOptions

--- src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,11 +1,36 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 
-const AvailableAppointment = ({selectedDate}) => {
+const AvailableAppointment = ({ selectedDate }) => {
+    const [treatmentOptions, setTreatmentOptions] = useState([])
+    const [treatment, setTreatment] = useState(null)
+     useEffect(() => {
+        fetch('treatmentData.json')
+            .then(res => res.json())
+            .then(data => setTreatmentOptions(data))
+
+    }, [])
     return (
         <section className='mt-16'>
             <h1 className='text-center text-blue-500  text-xl md:text-2xl lg:text-2xl font-bold'> Available Appointments on {format(selectedDate,'PP')} </h1>
-    
+     <div className='grid gap-4 py-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    treatmentOptions.map(treatmentOption => 
+                        <AppointmentOption
+                        
+                         key={treatmentOption._id}
+                        treatmentOption={treatmentOption}
+                            setTreatment={setTreatment}
+                        >
+                            
+
+                    </AppointmentOption>
+                    
+                    )
+                }
+
+            </div>
         </section>
     );
 };
