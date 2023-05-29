5291caffed601d87e41238aeff2877f069b7858e
Marjan-001 <marjan199721@gmail.com>
2023-05-29 21:08:44 +0600

    modal detail

--- src/pages/Appointment/BookingModal/BookingModal.js
+++ src/pages/Appointment/BookingModal/BookingModal.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const BookingModal = ({ treatment, selectedDate }) => {
     
-  const { name } = treatment;
+  const { name,slots } = treatment;
   const date = format(selectedDate,"PP")
     return (
         <>
@@ -14,7 +14,14 @@ const BookingModal = ({ treatment, selectedDate }) => {
                      <label htmlFor="booking-modal" className="btn btn-sm btn-circle bg-blue-700 absolute right-2 top-2">✕</label>
             <form className=' grid grid-cols-1 gap-1 justify-center items-center'>
               <input type="text"  value={date} className="input input-bordered mb-2 input-info w-full " disabled />
-              <input type="text" value={name} placeholder="Type here" className="input input-bordered mb-2 input-info w-full " disabled/>
+              <select className="select select-bordered select-info w-full ">
+  
+                {
+                  slots.map(slot =>
+                    <option value={slot}>{ slot}</option>
+                  )
+  }
+</select>
               <input type="text" placeholder="Type here" className="input input-bordered mb-2 input-info w-full " />
           <br/>
               <input className='w-full  btn bg-blue-600' type='submit' value="Submit" />
